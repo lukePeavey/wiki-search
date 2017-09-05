c6f9c0cc0104fc42a21791152808b10f61a4d8a4
lukePeavey <1williampeavey@gmail.com>
2017-09-05 05:13:42 -0400

    Update app component

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,13 +1,48 @@
 import React, { Component } from 'react'
+import SearchBar from '../search-bar/SearchBar'
+import SearchResults from '../search-results/SearchResults'
+import { isEmpty } from 'lodash'
+import { fetchSuggestions, fetchResults } from '../../api'
 import './App.css'
+import wikiLogo from '../../images/wikipedia.png'
 
 class App extends Component {
+  state = {
+    results: [],
+    value: '',
+    showingResults: false,
+    searchPlaceholder: 'Search Wikipedia...'
+  }
+
+  submitSearch = (event, { value }) => {
+    fetchResults(value)
+      .then(results => {
+        this.setState({ results, showingResults: true })
+        window.setTimeout(() => this.clearSearchBar(), 5) // hack
+      })
+      .catch(this.setState({ results: [] }))
+  }
+
+  clearSearchBar = () => {
+    this.textInput.clear()
+  }
+
   render() {
+    const { results, showingResults } = this.state
     return (
-      <div className="App">
-        <div className="App-header">
-          <h2>Wiki Search</h2>
-        </div>
+      <div className={`app ${showingResults ? 'showingResults' : ''}`}>
+        <section className="header">
+          <a href="/" className="home material-icons" />
+          <SearchBar
+            ref={(input) => { this.textInput = input }}
+            onSubmit={this.submitSearch}
+            onChange={() => {}}
+            placeholder={showingResults ? 'Search Wikipedia again...' : 'Search Wikipedia...'}
+          />
+        </section>
+        <section className="content">
+          <SearchResults results={results} />
+        </section>
       </div>
     )
   }
